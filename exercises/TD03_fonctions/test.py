fe2d15188af4ecaccce0ca8c2f98e994bdc71c53
Noémie KAUFMANN <noemie.kaufmann@ens.uvsq.fr>
2020-11-21 02:35:47 +0100

    bleh

--- exercises/TD03_fonctions/test.py
+++ exercises/TD03_fonctions/test.py
@@ -1,57 +1,42 @@
-# temps[0] : jours, temps[1]: heures, temps[2]: minutes, temps[3]: secondes
+def tempsEnDate(temps):
+    année = 1970+temps[0]//365
+    jour = temps[0]%365
+    heure = temps[1]
+    minute = temps[2]
+    seconde = temps[3]
+    return (année,jour,heure,minute,seconde)
 
-def tempsEnSeconde(temps):
-    """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
-    seconde = temps[0]*24*3600 + temps[1]*3600 + temps[2]*60 + temps[3]
-    return seconde
-
-
-def secondeEnTemps(seconde):
-    """Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument"""
-    j = seconde // (24*3600)
-    h = (seconde % (3600*24)) // 3600
-    m = (seconde % (3600*24)) % 3600 // 60
-    s = ((seconde % (3600*24)) % 3600) % 60
-    return (j, h, m, s)
-
-
-# fonction auxiliaire ici
 def pluriel(mot):
     """Renvoie la forme plurielle d'un mot càd rajoute un s"""
     return (mot+"s")
-
+    
+def secondeEnTemps(seconde):
+    """Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument"""
+    j = seconde//(24*3600)
+    h = (seconde%(3600*24))//3600
+    m = (seconde%(3600*24))%3600//60
+    s = ((seconde%(3600*24))%3600)%60
+    return (j,h,m,s)
 
 def afficheTemps(temps):
-    mot = ["jour", "heure", "minute", "seconde"]
+    """fonction d'affichage du temps en j, h, min, sec"""
+    mot=["jour","heure","minute","seconde"]
     for i in range(4):
-        if temps[i] > 1:
-            return (temps[i], pluriel(mot[i]))
-        elif temps[i] == 1:
-            return (temps[i], mot[i])
+        if temps[i] >1 :
+            print (temps[i], pluriel(mot[i])," ", end = '')
+        elif temps[i] ==1 :
+            print  (temps[i], mot[i]," ", end = '')   
 
+def afficheDate(date = -1):
+    mot=["année","jour","heure","minute","seconde"]
+    for i in range(5):
+        if tempsEnDate[i] >1 :
+            print (tempsEnDate[i], pluriel(mot[i])," ", end = '')
+        elif tempsEnDate[i] ==1 :
+            print  (temps[i], mot[i]," ", end = '')
 
-def demandeTemps():
-    """demande de rentrer un nb de j, h ,min, sec"""
-    j = int(input("how many days?"))
-    h = int(input("how many hours?"))
-    while h > 23:
-        print("wrong. do it again")
-        h = int(input())
-    m = int(input("how many minutes?"))
-    while m > 59:
-        print("wrong. do it again")
-        m = int(input())
-    s = int(input("and how many seconds?"))
-    while s > 59:
-        print("wrong. do it again")    
-        s = int(input())    
-    return (j, h, m, s)
-
-
-def sommeTemps(temps1, temps2):
-    s = tempsEnSeconde(temps1)+tempsEnSeconde(temps2)
-    t = secondeEnTemps(s)
-    return t
-
-
-sommeTemps((2, 3, 4, 25), (5, 22, 57, 1)) 
+temps = secondeEnTemps(1000000000)
+afficheTemps(temps)
+afficheDate(tempsEnDate(temps))
+afficheDate()            
+            
